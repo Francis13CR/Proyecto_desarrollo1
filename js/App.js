b5095420bb3437f450299aea7ed63b5a8b97c28e
Francis Meléndez <francismelendez134@gmail.com>
2021-10-24 18:28:29 -0600

    formulario envio

--- js/App.js
+++ js/App.js
@@ -1,7 +1,21 @@
 
+//temporizador
+let segundos =30;
 
+function temporizador(){
+   
+    segundos --;
+     if(segundos==0){
+        window.location.href = "index.html";
+    }
+    
+}
 
 //formulario de envio de lugar 
+
+const alerta = document.getElementById("alerta");
+alerta.style.visibility = 'hidden';
+//console.log(alerta);
 
 const formulario = document.querySelector('form');
 formulario.addEventListener('submit', event => {
@@ -15,11 +29,27 @@ formulario.addEventListener('submit', event => {
     var categories = document.getElementById("categorias")
     var seleccionado = categories.options[categories.selectedIndex].value;
     
-    console.log(autor + titulo + seleccionado + description);
-    alert(`Su formulario ha sido enviado con los siguientes datos autor: ${autor}, titulo: ${titulo}, descripcion: ${description}, categoria ${seleccionado}... Redireccionando a la pagina principal` );
-    setTimeout(function(){
-        window.location.href = "index.html";
-    }, 10);
+    //console.log(autor + titulo + seleccionado + description);
     document.querySelector('form').reset();
+    setInterval(function() {
+         alerta.innerHTML=`Su formulario ha sido enviado con los siguientes datos <br>  -autor: ${autor}, <br> -titulo: ${titulo}, <br> -descripcion: ${description}, <br> -categoria ${seleccionado} <br> <br> Redireccionando a la pagina principal en ${segundos}s `;
+    alerta.style.visibility = 'visible';
+    temporizador();
+   
+    }, 1000)
+   
+    //alert(`Su formulario ha sido enviado con los siguientes datos autor: ${autor}, titulo: ${titulo}, descripcion: ${description}, categoria ${seleccionado}... Redireccionando a la pagina principal` );
+   // setTimeout(function(){
+  //      window.location.href = "index.html";
+   // }, 5000);
+    
 
 })
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    gsap.to(".logo", {
+        duration: 1,
+        left: 210,
+        delay: 5
+    });
+});
