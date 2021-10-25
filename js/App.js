33b6e2bdd0991a4ea7c35c566d330f8e9597fc58
Francis Meléndez <francismelendez134@gmail.com>
2021-10-24 20:33:35 -0600

    avance formulario sing

--- js/App.js
+++ js/App.js
@@ -10,15 +10,45 @@ function temporizador() {
 
 }
 
+//formulario sing up
+
+function singUp(){
+var singUp = document.getElementById('singUp');
+singUp.addEventListener('submit',event => {
+    event.preventDefault();
+
+    const alerta = document.getElementById("alerta-singup");
+    alerta.style.visibility = 'visible';
+
+
+    var name = document.getElementById("full-name").value;
+    var email = document.getElementById("email").value;
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    var passwordValidation = document.getElementById("passwordValidation").value;
+
+   console.log(name);
+    alerta.innerHTML = `Usuario registrado correctamente <br>  -name: ${name}, <br> -email: ${email}, <br> -username: ${username}, <br> -password ${password} <br> <br> Redireccionando a la pagina principal en ${segundos}s `;
+    alerta.style.visibility = 'visible';
+     gsap.to("#alerta-singup", {
+        duration: .8,
+        y: 950,
+        ease: 'bounce'
+    });
+})
+}
+
+
 //formulario de envio de lugar 
 
-const alerta = document.getElementById("alerta");
-alerta.style.visibility = 'visible';
+
 //console.log(alerta);
 
-const placeForm = document.getElementById('placeForm');
+var placeForm = document.getElementById('placeForm');
 placeForm.addEventListener('submit', event => {
     event.preventDefault();
+    const alerta = document.getElementById("alerta-place");
+    alerta.style.visibility = 'hidden';
 
     var autor = document.getElementById('autor').value;
     var titulo = document.getElementById('titulo').value;
@@ -33,7 +63,7 @@ placeForm.addEventListener('submit', event => {
     alerta.innerHTML = `Su formulario ha sido enviado con los siguientes datos <br>  -autor: ${autor}, <br> -titulo: ${titulo}, <br> -descripcion: ${description}, <br> -categoria ${seleccionado} <br> <br> Redireccionando a la pagina principal en ${segundos}s `;
     alerta.style.visibility = 'visible';
 
-    gsap.to("#alerta", {
+    gsap.to("#alerta-place", {
         duration: .8,
         y: 950,
         ease: 'bounce'
@@ -44,3 +74,4 @@ placeForm.addEventListener('submit', event => {
 
     }, 1000)
 })
+
