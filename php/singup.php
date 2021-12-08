24662d2ca4836e5bf01dc1e783b02cbc0b8466ef
Francis Meléndez <francismelendez134@gmail.com>
2021-12-07 18:42:06 -0600

    arreglo sinup

--- php/singup.php
+++ php/singup.php
@@ -2,7 +2,7 @@
 
 namespace Medoo;
 
-require './php/Medoo.php';
+require 'Medoo.php';
 
 $database = new Medoo([
     'type' => 'mysql',
@@ -22,7 +22,7 @@ $database = new Medoo([
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/singup.css">
+    <link rel="stylesheet" href="../css/singup.css">
     <title>Sing up</title>
 </head>
 
